7413d48603d4c85142785c7d38023dcfcb5f22fb
alirajgarah <77947215+alirajgarah@users.noreply.github.com>
2021-02-10 01:37:37 -0600

    Add files via upload

---
+++ 2.19CIS2348.py
@@ -0,0 +1,25 @@
+# 1 \\\
+lemon_juice = float(input('Enter amount of lemon juice (in cups):\n'))
+water_juice = float(input('Enter amount of water (in cups):\n'))
+agave_nectar = float(input('Enter amount of agave nectar (in cups):\n'))
+servings_size = float(input('How many servings does this make?\n\n'))
+
+
+print('Lemonade ingredients - yields {:.2f} servings'.format(servings_size))
+print('{:.2f} cup(s) lemon juice'.format(lemon_juice))
+print('{:.2f} cup(s) water'.format(water_juice))
+print('{:.2f} cup(s) agave nectar\n'.format(agave_nectar))
+
+# 2 \\\
+desired_servings = float(input('How many servings would you like to make?\n\n'))
+
+print('Lemonade ingredients - yields {:.2f} servings'.format(desired_servings))
+print('{:.2f} cup(s) lemon juice'.format(lemon_juice * desired_servings / servings_size))
+print('{:.2f} cup(s) water'.format(water_juice * desired_servings / servings_size))
+print('{:.2f} cup(s) agave nectar\n'.format(agave_nectar * desired_servings / servings_size))
+
+# 3 \\\
+print('Lemonade ingredients - yields {:.2f} servings'.format(desired_servings))
+print('{:.2f} gallon(s) lemon juice'.format(lemon_juice * desired_servings / servings_size / 16))
+print('{:.2f} gallon(s) water'.format(water_juice * desired_servings / servings_size / 16))
+print('{:.2f} gallon(s) agave nectar'.format(agave_nectar * desired_servings / servings_size / 16))
---
+++ 5.19CIS2348.py
@@ -0,0 +1,32 @@
+def CostofService(s):
+    if s == "Oil change":
+        return 35
+    if s == "Tire rotation":
+        return 19
+    if s == "Car wash":
+        return 7
+    if s == "Car wax":
+        return 12
+
+print("Davy's auto shop services\n"
+      "Oil change -- $35\n"
+      "Tire rotation -- $19\n"
+      "Car wash -- $7\n"
+      "Car wax -- $12\n")
+
+service_one = input("Select first service:\n")
+service_two = input("Select second service:\n")
+
+print("\nDavy's auto shop invoice\n")
+
+firstinput = CostofService(service_one)
+secondinput = 0
+print("Service 1: %s, $%d" % (service_one, firstinput))
+
+if service_two == '-':
+    print("Service 2: No service")
+else:
+    two = CostofService(service_two)
+print("Service 2: %s, $%d" % (service_two, secondinput))
+
+print("\nTotal: $%d" % (firstinput + secondinput))
